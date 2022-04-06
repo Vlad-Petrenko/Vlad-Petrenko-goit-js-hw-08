b7f5fca5b22723fbec6dd7cbc7442807ce4c78b6
Vlad-Petrenko <vlad120595@gmail.com>
2022-04-06 22:50:32 +0300

    8я работа

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+function onPlay(data) {
+  let timePlayVideo = data.seconds;
+
+  localStorage.setItem('videoplayer - current - time', timePlayVideo);
+}
+haveCurrentTime();
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function haveCurrentTime() {
+  let savedCurrentTime = localStorage.getItem('videoplayer - current - time');
+
+  if (savedCurrentTime) {
+    player.setCurrentTime(savedCurrentTime).catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+        default:
+          break;
+      }
+    });
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form  textarea'),
+  emailText: document.querySelector('.feedback-form input'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+refs.form.addEventListener('submit', addFormSubmit);
+
+refs.form.addEventListener('input', throttle(addTextAndEmailInput, 500));
+
+feedbackTextArea();
+
+function addTextAndEmailInput(event) {
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function addFormSubmit(event) {
+  event.preventDefault();
+  console.log(formData);
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function feedbackTextArea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const parsedMessage = JSON.parse(savedMessage);
+  if (parsedMessage) {
+    refs.textarea.value = parsedMessage.message;
+    refs.emailText.value = parsedMessage.email;
+  }
+}
